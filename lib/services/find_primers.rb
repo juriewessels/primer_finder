b29970be4930fab34a89e5def5733bf5d2a1cacc
Jurie Wessels <jurie@juriewessels.com>
2022-01-01 20:43:52 -0800

    Readd 9222 debugging port

--- lib/services/find_primers.rb
+++ lib/services/find_primers.rb
@@ -47,12 +47,11 @@ class FindPrimers
     if chrome_bin = ENV['GOOGLE_CHROME_BIN']
       options.add_argument('--no-sandbox')
       options.add_argument('--disable-dev-shm-usage')
+      options.add_argument('--remote-debugging-port=9222')
       options.binary = chrome_bin
     end
 
     options.add_argument('--headless')
-    # options.add_argument('--disable-gpu')
-    # options.add_argument('--remote-debugging-port=9222')
 
 
     Selenium::WebDriver.logger.level = :debug
